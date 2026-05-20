20f045f93957083e1a34000bedc94e4aea367c42
Flur17 <kaufmannfelix3@gmail.com>
2026-05-20 09:13:22 +0200

    uhm

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld{ 
     public static void main(String[] args) {
-        System.out.println("Hello World hallo");
+        System.out.println("Hello World sigma hallo");
     }
 }
